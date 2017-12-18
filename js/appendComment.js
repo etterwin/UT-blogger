3fbf700eb06d5ed04adf2644082cf800a7aa2276
Dasha <etterwin.yll@gmail.com>
2017-12-18 16:52:03 +0300

    update

---
+++ js/appendComment.js
@@ -0,0 +1,21 @@
+function addComment() {
+    let text = document.getElementById('user_text').value;
+
+    let Data = new Date();
+
+    let Year = Data.getFullYear();
+    let Month = Data.getMonth();
+    let Day = Data.getDay();
+
+    let Hour = Data.getHours();
+    let Minutes = Data.getMinutes();
+
+
+    document.getElementById('text').append(text);
+    document.getElementById('time').append(Hour+':'+Minutes);
+    document.getElementById('date').append(Day+'/'+Month+'/'+Year);
+    document.getElementById('comment').classList.toggle('comment');
+
+    let text = document.getElementById('user_text').value;
+
+}
