f046714617e082c8f868400beda2e3c0bbe3df77
Dasha <etterwin.yll@gmail.com>
2017-12-18 17:36:00 +0300

    update

--- js/appendComment.js
+++ js/appendComment.js
@@ -1,20 +1,27 @@
+let counter = 0;
+
 function addComment() {
 
     let text = document.getElementById('user_text').value;
+    counter++;
 
-    let Data = new Date();
+    if(counter<=1) {
 
-    let Year = Data.getFullYear();
-    let Month = Data.getMonth();
-    let Day = Data.getDay();
+        let Data = new Date();
 
-    let Hour = Data.getHours();
-    let Minutes = Data.getMinutes();
+        let Year = Data.getFullYear();
+        let Month = Data.getMonth();
+        let Day = Data.getDay();
 
+        let Hour = Data.getHours();
+        let Minutes = Data.getMinutes();
 
-    document.getElementById('text').append(text);
-    document.getElementById('time').append(Hour+':'+Minutes);
-    document.getElementById('date').append(Day+'/'+Month+'/'+Year);
-    document.getElementById('comment').classList.toggle('comment');
+        document.getElementById('text').append(text);
+        document.getElementById('time').append(Hour+':'+Minutes);
+        document.getElementById('date').append(Day+'/'+Month+'/'+Year);
+        document.getElementById('comment').classList.add('comment').remove('comment--none');
+    } else {
+        return false;
+    }
 
 }
